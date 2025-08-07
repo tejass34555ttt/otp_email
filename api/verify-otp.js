8efd3fef88f40a52d6a8e2c4dc7088562da135a6
Tejas H Shinde <tejasshinde4545k@gmail.com>
2025-08-07 15:24:59 +0530

    Update verify-otp.js

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -5,29 +5,7 @@ const { saveOtp, verifyOtp, clearOtp } = require('../utilis/otp_store');
 const { users } = require('../services/appwriteClient');
 const { ID, Query } = require('node-appwrite');
 
-// ------------------- SEND OTP -------------------
-router.post('/send-otp', async (req, res) => {
-  const rawEmail = req.body.email;
-  const email = rawEmail.trim().toLowerCase(); // normalize email
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
-  console.log(`✅ Sending OTP to ${email}: ${otp}`);
-  saveOtp(email, otp);
-
-  try {
-    await transporter.sendMail({
-      from: `"Appwrite OTP" <${process.env.SMTP_USERNAME}>`,
-      to: email,
-      subject: 'Your OTP Code',
-      text: `Your OTP is: ${otp}`,
-    });
-
-    res.json({ message: 'OTP sent successfully' });
-  } catch (error) {
-    console.error(`❌ Failed to send OTP to ${email}:`, error.message);
-    res.status(500).json({ error: 'Failed to send OTP', details: error.message });
-  }
-});
 
 // ------------------- VERIFY OTP & LOGIN -------------------
 router.post('/verify-otp', async (req, res) => {
