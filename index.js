e0d01dea065234b528b552f62a94e2a2fed04108
Tejas H Shinde <tejasshinde4545k@gmail.com>
2025-08-07 15:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: '*', // or specify only the allowed origin: 'http://localhost:52293'
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(cors());
 app.use(bodyParser.json());
 
