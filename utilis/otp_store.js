53579175adba30d4ca630c0e73fc95b52cb7c500
Tejas H Shinde <tejasshinde4545k@gmail.com>
2025-08-07 14:29:26 +0530

    Update otp_store.js

--- utilis/otp_store.js
+++ utilis/otp_store.js
@@ -2,7 +2,7 @@ import { Client, Databases, ID, Query } from "node-appwrite";
 
 const client = new Client()
   .setEndpoint(process.env.APPWRITE_ENDPOINT)
-  .setProject(process.env.APPWRITE_PROJECT_ID)
+  .setProject(process.env.APPWRITE_PROJECT)
   .setKey(process.env.APPWRITE_API_KEY);
 
 const databases = new Databases(client);
